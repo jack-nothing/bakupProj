fea62f9e808d3b98ce77893ee85a4e893dd21336
xiekun <857812506@qq.com>
2019-08-23 16:27:01 +0800

    quartz

---
+++ src/main/java/com/kechun/controller/ScheduledController.java
@@ -0,0 +1,13 @@
+package com.kechun.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("scheduled")
+public class ScheduledController {
+
+
+
+}
--- src/main/java/com/kechun/scheduled/Monitor.java
+++ src/main/java/com/kechun/scheduled/Monitor.java
@@ -5,14 +5,35 @@ import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @Component
 public class Monitor {
     private static final Logger LOG = LoggerFactory.getLogger(Monitor.class);
 
 
-
-    @Scheduled(cron = "59 59 23 * * ? ")
+    /**
+     * 30分钟一次，监控
+     */
+    @Scheduled(cron = "* 0/30 * * * ? ")
     public void monitor() {
-        LOG.info("bakUp_xxxx");
+        SimpleDateFormat sdf = new SimpleDateFormat("YYYYMMdd");
+        String curDay = sdf.format(new Date());
+        LOG.info("start:");
+        Runtime runtime = Runtime.getRuntime();
+        Process process = null;
+        try {
+            StringBuffer sbf = new StringBuffer();
+            process = runtime.exec(new String[]{
+                    "/bin/sh",
+                    "-c",
+                    sbf.append("ls ").toString()
+                    });
+        }catch (Exception e){
+            e.printStackTrace();
+            LOG.info(e.getMessage());
+        }
+
     }
 }
